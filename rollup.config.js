ee2c2d9677c93cc1708eab7d77e812495565e788
icepro <775846939@qq.com>
2021-10-11 22:59:51 +0800

    feat: add build for umd version
    add build for umd version
    add "module" in package.json

--- rollup.config.js
+++ rollup.config.js
@@ -1,22 +1,44 @@
 import typescript from 'rollup-plugin-typescript2';
 import sourceMaps from 'rollup-plugin-sourcemaps';
-export default {
-  input: './src/index.ts',
-  plugins: [
-    typescript({
-      exclude: 'node_modules/**',
-      typescript: require('typescript'),
-    }),
-    sourceMaps(),
-  ],
-  output: [
-    {
+import { getBabelOutputPlugin } from '@rollup/plugin-babel';
+
+const plugins = [
+  typescript({
+    exclude: 'node_modules/**',
+    typescript: require('typescript'),
+  }),
+  sourceMaps(),
+];
+const input = './src/index.ts';
+
+export default [
+  {
+    input,
+    plugins,
+    output: {
       format: 'cjs',
       file: 'lib/remoteAsync.cjs.js',
     },
-    {
+  },
+  {
+    input,
+    plugins: [
+      ...plugins,
+      getBabelOutputPlugin({ presets: [['@babel/env', { modules: 'umd' }]],  allowAllFormats: true, }),
+    ],
+    output: {
+      format: 'umd',
+      file: 'lib/remoteAsync.umd.js',
+
+      name: 'remoteAsync',
+    },
+  },
+  {
+    input,
+    plugins,
+    output: {
       format: 'es',
       file: 'lib/remoteAsync.esm.js',
     },
-  ],
-};
+  },
+];
